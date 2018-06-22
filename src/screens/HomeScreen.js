a5f4ac3aadfb5358056c06f305a3f78ed6d1a344
hayo0914 <hayo0914@gmail.com>
2018-06-22 15:58:23 +0900

    Fix eslint settings and added navigator package

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => (
+  <View style={{ flex: 1 }}>
+    <Text>Hello</Text>
+  </View>
+);
+
+export default HomeScreen;
