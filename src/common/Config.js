7035cbd2a9c9b59c9a210e3838a47450d41395e7
hayo0914 <hayo0914@gmail.com>
2018-07-03 18:57:38 +0900

    Rewrite for publish

--- src/common/Config.js
+++ src/common/Config.js
@@ -1,12 +1,12 @@
 export default {
   // Firebase
-  API_KEY: 'AIzaSyCCTBtJeAqoamDzT4LfKSQEfxmYhpvj9sM',
-  AUTH_DOMAIN: 'rn-paintings.firebaseapp.com',
-  DATABASE_URL: 'https://rn-paintings.firebaseio.com',
-  PROJECT_ID: 'rn-paintings',
-  STORAGE_BUCKET: 'rn-paintings.appspot.com',
-  MESSAGING_SENDER_ID: '465400638321',
+  API_KEY: '**********',
+  AUTH_DOMAIN: '**********',
+  DATABASE_URL: '**********',
+  PROJECT_ID: '**********',
+  STORAGE_BUCKET: '**********',
+  MESSAGING_SENDER_ID: '**********',
 
   // Facebook
-  FACEBOOK_APP_ID: '393602067714578',
+  FACEBOOK_APP_ID: '**********',
 };
