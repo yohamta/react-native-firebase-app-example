9392dd36d3194f7dd55298624dddfa14b17dc3b2
hayo0914 <hayo0914@gmail.com>
2018-07-03 19:04:01 +0900

    Fix config

--- src/common/Config.js
+++ src/common/Config.js
@@ -1,12 +1,12 @@
 export default {
   // Firebase
-  API_KEY: '**********',
-  AUTH_DOMAIN: '**********',
-  DATABASE_URL: '**********',
-  PROJECT_ID: '**********',
-  STORAGE_BUCKET: '**********',
-  MESSAGING_SENDER_ID: '**********',
+  API_KEY: 'AIzaSyCCTBtJeAqoamDzT4LfKSQEfxmYhpvj9sM',
+  AUTH_DOMAIN: 'rn-paintings.firebaseapp.com',
+  DATABASE_URL: 'https://rn-paintings.firebaseio.com',
+  PROJECT_ID: 'rn-paintings',
+  STORAGE_BUCKET: 'rn-paintings.appspot.com',
+  MESSAGING_SENDER_ID: '465400638321',
 
   // Facebook
-  FACEBOOK_APP_ID: '**********',
+  FACEBOOK_APP_ID: '393602067714578',
 };
